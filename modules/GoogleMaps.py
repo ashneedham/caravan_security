fc71bcd7df90d3948710d5f077a1c8a67f2635aa
Ashley Needham <ash.needham87@gmail.com>
2017-01-21 11:18:45 +0000

    Google maps class to handle lat long co-ords with google maps.
    Currently just had 1 method for returning a url

---
+++ modules/GoogleMaps.py
@@ -0,0 +1,17 @@
+class GoogleMaps():
+    lat = '0.0'
+    long = '0.0'
+
+    def __init__(self, lat, long):
+        self.lat = lat
+        self.long = long
+
+    def getMapsUrl(self):
+        """
+        Returns a url to google maps with pin on the specified lat long co-ordinates
+        :return:
+        :rtype: str
+        """
+        return 'http://maps.google.com/maps?z=12&t=m&q=loc:' + str(self.lat) + '+' + str(self.long)
+
+
