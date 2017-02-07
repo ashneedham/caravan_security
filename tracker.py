51a0a0dcb9add534a9454225ee129d6cbd1b6aac
Ashley Needham <ash.needham87@gmail.com>
2017-02-07 21:16:36 +0000

    Updated tracker.py to handle sending link via sms

--- tracker.py
+++ tracker.py
@@ -1,5 +1,6 @@
 from modules.Gps import Gps
 from modules.GoogleMaps import GoogleMaps
+from modules.Sms import Sms
 import sys
 
 print('Booting GPS...')
@@ -8,6 +9,10 @@ latLong = gpsObj.getCurrentLocation()
 print(latLong)
 mapObj = GoogleMaps(latLong['lat'], latLong['long'])
 print(mapObj.getMapsUrl())
+modem = Sms()
+print('Sending SMS...')
+modem.sendSMS(mapObj.getMapsUrl(),'07846347904')
 del gpsObj
 del mapObj
+del modem
 sys.exit()
