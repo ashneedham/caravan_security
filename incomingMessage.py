0e9fb6834577b6adae3722ff514463c53055e092
Ashley Needham <ash.needham87@gmail.com>
2017-02-07 21:16:04 +0000

    New script for handling an incoming message

---
+++ incomingMessage.py
@@ -0,0 +1,45 @@
+from modules.Gps import Gps
+from modules.GoogleMaps import GoogleMaps
+from modules.Sms import Sms
+import os
+import logging
+import datetime
+
+def getMessage():
+    number = os.environ['SMS_1_NUMBER']
+
+    # Are there any decoded parts?
+    numparts = int(os.environ['DECODED_PARTS'])
+
+    if numparts:
+        # Get all text parts
+        text = ''
+        for i in range(0, numparts):
+            varname = 'DECODED_%d_TEXT' % i
+            if varname in os.environ:
+                text = text + os.environ[varname]
+    else:
+        text = os.environ['SMS_1_TEXT']
+
+    text = text.decode('UTF-8')
+    return number, text
+
+def handleCommand(text, number):
+
+    if text.lower().strip() == 'where are you':
+        gpsObj = Gps()
+        latLong = gpsObj.getCurrentLocation()
+        mapObj = GoogleMaps(latLong['lat'], latLong['long'])
+        modem = Sms()
+        modem.sendSMS(mapObj.getMapsUrl(), number)
+
+        del gpsObj
+        del mapObj
+        del modem
+
+
+number, text = getMessage()
+handleCommand(text, number)
+
+logging.basicConfig(filename='/home/pi/Projects/CaravanSecurity/Source/example.log',level=logging.DEBUG)
+logging.info('incomingMessage ran ' + str(datetime.date.today()) + ' ' + str(datetime.datetime.now().time()) + ' ' + number + ' ' + text)
